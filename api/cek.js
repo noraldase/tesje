3c5ed8aae9312b1764e4a8feab59eba4bd937950
noraldase <norald711@gmail.com>
2026-02-22 21:50:02 +0700

    Update cek.js

--- api/cek.js
+++ api/cek.js
@@ -1,5 +1,4 @@
 export default async function handler(req, res) {
-  // Hanya izinkan metode POST
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method Not Allowed' });
   }
@@ -10,17 +9,27 @@ export default async function handler(req, res) {
     const response = await fetch('https://open-api.hidupcuan.org/api/player/get', {
       method: 'POST',
       headers: {
+        'Accept': 'application/json',
+        'Accept-Language': 'en-US,en;q=0.9',
         'Content-Type': 'application/json',
         'Origin': 'https://toko.hidupcuan.org',
         'Referer': 'https://toko.hidupcuan.org/',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+        'Sec-Ch-Ua': '"Not:A-Brand";v="99", "Brave";v="145", "Chromium";v="145"',
+        'Sec-Ch-Ua-Mobile': '?0',
+        'Sec-Ch-Ua-Platform': '"Windows"',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-site',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36'
       },
       body: JSON.stringify({ "PlayerID": PlayerID.toString() })
     });
 
     const data = await response.json();
+    
+    // Kirim hasil kembali ke frontend kamu
     res.status(200).json(data);
   } catch (error) {
-    res.status(500).json({ status: "error", message: error.message });
+    res.status(500).json({ status: "error", message: "Gagal menyambung ke server pusat" });
   }
 }
